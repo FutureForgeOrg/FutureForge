c92477795783ef487fec78dd83dc51b9ca2d9393
ParamPS25 <bhavsarparam1940@gmail.com>
2025-07-15 19:59:51 +0530

    feat : implemented redis setup and job controller to serve scrapped jobs with including pagination,filtering and searching

---
+++ backend/utils/redis.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+redisClient.on('error', (err) => console.error('Redis Client Error:', err));
+
+await redisClient.connect();
+
+export default redisClient;
