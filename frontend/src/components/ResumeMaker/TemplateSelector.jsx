169ff9e522e9f8eb4adbda4a496436abb3e59c78
Patel purv <patelpurv2643@gmail.com>
2025-08-04 21:22:01 +0530

    feat:resume builder work start and added some sample

---
+++ frontend/src/components/ResumeMaker/TemplateSelector.jsx
@@ -0,0 +1,17 @@
+export default function TemplateSelector({ selected, setSelected }) {
+  return (
+    <div className="flex justify-center gap-4">
+      {[1, 2 ,3].map((template) => (
+        <button
+          key={template}
+          onClick={() => setSelected(template)}
+          className={`px-4 py-2 border rounded ${
+            selected === template ? "bg-blue-600 text-white" : ""
+          }`}
+        >
+          Template {template}
+        </button>
+      ))}
+    </div>
+  );
+}
--- frontend/src/pages/tools/Resume.jsx
+++ frontend/src/pages/tools/Resume.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import ResumeForm from '../../components/ResumeMaker/ResumeForm'
+import TemplateSelector from '../../components/ResumeMaker/TemplateSelector'
+import ResumePreview from '../../components/ResumeMaker/ResumePreview'
+import { useState } from 'react';
+import { exportToPDF } from '../../components/ResumeMaker/pdfExporter';
 function Resume() {
+  const [formData, setFormData] = useState({
+    name: "",
+  email: "",
+  phone: "",
+  address: "",
+  skills: "",
+  education: [""],
+  experience: "",
+  projects: [""],
+  certificates: [""],
+  links: "",
+  });
+
+  const [selectedTemplate, setSelectedTemplate] = useState(1);
+  const previewRef=useRef();
   return (
-    <div>Resume</div>
+    <>
+      <div p-y space-y-8>
+        <h1 className="text-3xl font-bold text-center">Resume Builder</h1>
+        <ResumeForm formData={formData} setFormData={setFormData} />
+        <TemplateSelector
+          selected={selectedTemplate}
+          setSelected={setSelectedTemplate}
+        />
+        <div className="border p-4 rounded shadow" ref={previewRef}>
+        <ResumePreview selected={selectedTemplate} data={formData} />
+      </div>
+      <div className="text-center">
+        <button
+          onClick={() => exportToPDF(previewRef.current)}
+          className="bg-blue-600 text-white px-4 py-2 rounded"
+        >
+          Download PDF
+        </button>
+      </div>
+      </div>
+    </>
   )
 }
 
